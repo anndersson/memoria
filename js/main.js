63d58bc1eff845b150d772b9dc80e88bfc918324
Andersson <aonavas@scl.edu.gt>
2022-08-12 10:17:07 -0600

    finish

--- js/main.js
+++ js/main.js
@@ -1,43 +1,65 @@
-let root = document.querySelector("#root");
 
+let iconos = []
+        let selecciones = []
 
-// BASE DE DATOS
-let frutas = ['🍇','🍈','🍉','🍊','🍋','🍌','🍍','🥭','🍎'];
-let frutas2 = ['🍇','🍈','🍉','🍊','🍋','🍌','🍍','🥭','🍎'];
+        generarTablero()
 
-let todas_las_frutas = frutas.concat(frutas2);
+        function cargarIconos() {
+            iconos = [
+                '🍇','🍈','🍉','🍊','🍋','🍌','🍍','🥭','🍎'
+            ]
+        }
 
+        function generarTablero() {
+            cargarIconos()
+            selecciones = []
+            let tablero = document.getElementById("tablero")
+            let tarjetas = []
+            for (let i = 0; i < 18; i++) {
+                tarjetas.push(`
+                <div class="area-tarjeta" onclick="seleccionarTarjeta(${i})">
+                    <div class="tarjeta" id="tarjeta${i}">
+                        <div class="cara trasera" id="trasera${i}">
+                            ${iconos[0]}
+                        </div>
+                        <div class="cara superior">
+                            <i class="far fa-question-circle"></i>
+                        </div>
+                    </div>
+                </div>        
+                `)
+                if (i % 2 == 1) {
+                    iconos.splice(0, 1)
+                }
+            }
+            tarjetas.sort(() => Math.random() - 0.5)
+            tablero.innerHTML = tarjetas.join(" ")
+        }
 
-function agregar_datos_al_DOM(){
+        function seleccionarTarjeta(i) {
+            let tarjeta = document.getElementById("tarjeta" + i)
+            if (tarjeta.style.transform != "rotateY(180deg)") {
+                tarjeta.style.transform = "rotateY(180deg)"
+                selecciones.push(i)
+            }
+            if (selecciones.length == 2) {
+                deseleccionar(selecciones)
+                selecciones = []
+            }
+        }
 
-
-    todas_las_frutas.forEach(function(elemento,indice){
-
-        let contenedor = document.createElement("div");
-
-        contenedor.innerHTML = 
-        '<div class="fruta">' +
-        elemento +
-        '</div>';
-
-        root.appendChild(contenedor);
-
-    });
-
-}
-agregar_datos_al_DOM();
-
-
-// FUNCIÓN DE SELECCIÓN
-
-let datos_del_dom = document.querySelectorAll(".fruta");
-
-function activar(){
-    this.classList.add("activar");
-}
-
-datos_del_dom.forEach(function(elemento, indice){
-    
-    elemento.addEventListener('click', activar);
-
-});
+        function deseleccionar(selecciones) {
+            setTimeout(() => {
+                let trasera1 = document.getElementById("trasera" + selecciones[0])
+                let trasera2 = document.getElementById("trasera" + selecciones[1])
+                if (trasera1.innerHTML != trasera2.innerHTML) {
+                    let tarjeta1 = document.getElementById("tarjeta" + selecciones[0])
+                    let tarjeta2 = document.getElementById("tarjeta" + selecciones[1])
+                    tarjeta1.style.transform = "rotateY(0deg)"
+                    tarjeta2.style.transform = "rotateY(0deg)"
+                }else{
+                    trasera1.style.background = "rgb(221, 49, 19)"
+                    trasera2.style.background = "rgb(221, 49, 19)"
+                }
+            }, 1000);
+        }
